84c611de45eaa17dc4e4d408e326507f2e2e09d1
Mike Rodsky <mrodsky@gmail.com>
2018-07-08 09:32:52 -0400

    new person object

--- test.js
+++ test.js
@@ -15,8 +15,14 @@ function double (something) {
   color: "red"
 };
 
-var x = car;
+var person = new Object();
+person.firstName = "Michael";
+person.lastName = "Rodsky";
+person.age = 30;
+person.eyeColor = "brown";
 
+var x = car;
+console.log(person.firstName + " drives a ")
 console.log(x.make);
 console.log(x.model);
 console.log(double(x.mpg));
